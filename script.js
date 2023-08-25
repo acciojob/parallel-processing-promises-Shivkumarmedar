45f88c7f084b5e606b75853eb86b7e3687f85fa9
Shivkumarmedar <121854125+Shivkumarmedar@users.noreply.github.com>
2023-08-25 19:08:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-//your JS code here. If required.
-const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
-
 const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
-];
+      { url: 'image1.jpg' },
+      { url: 'image2.jpg' },
+      { url: 'image3.jpg' }
+      // Add more image URLs as needed
+    ];
+
+    function loadImage(url) {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => resolve(img);
+        img.onerror = () => reject(new Error(`Failed to load image's URL: ${url}`));
+        img.src = url;
+      });
+    }
+
+ document.getElementById('download-images-button').addEventListener('click', async () => {
+      const outputDiv = document.getElementById('output');
+      outputDiv.innerHTML = ''; // Clear previous images
+
+      try {
+        const imageElements = await Promise.all(images.map(image => loadImage(image.url)));
+        
+        imageElements.forEach(imageElement => {
+          outputDiv.appendChild(imageElement);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    });
